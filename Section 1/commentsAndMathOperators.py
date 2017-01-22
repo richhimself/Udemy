9e5700710d78dccb6a0e23778dbb797df2199a08
richhimself <rich@richhimself.com>
2017-01-22 18:28:50 +0100

    Uplaopd

---
+++ Section 1/commentsAndMathOperators.py
@@ -0,0 +1,19 @@
+#This is a single line comment
+
+"""This is a multiline comment
+
+this is form the udemy section 2.6"""
+
+add = 10 + 22
+sub = 10 - 50
+mult = 25 * 5
+div =  872 / 88
+power = 3 ** 3
+mod = 10 % 3
+
+
+ordOp1 = 7 + 6 + 9 - 4 * ((9 - 2) ** 2) / 7
+ordOp2 = (6 % 4 * (7 + (7 + 2) * 3)) ** 2
+
+print(ordOp1)
+print(ordOp2)
---
+++ Section 1/variablesAndDataTypes.py
@@ -0,0 +1,14 @@
+variableNamesCannotBeginWithANumber = True
+
+initial = 3
+
+variableboolean = False
+
+posint = 10
+
+zero = 0
+
+negFloat = -9.143
+
+initial = 18
+
