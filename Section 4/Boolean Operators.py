06feb8825465f7e42c0b4ed10e42b45aa179dd3b
richhimself <rich@richhimself.com>
2017-02-21 20:27:35 +0100

    Upload

---
+++ Section 4/Boolean Operators.py
@@ -0,0 +1,23 @@
+var1 = True and True
+var2 = False and False
+var3 = True or True
+var4 = False or False
+var5 = not False
+var6 = not True
+
+print(var1)
+print(var2)
+print(var3)
+print(var4)
+print(var5)
+print(var6)
+
+#var10 = not 3>1 and 5 != 2 and 6=6
+
+#print(var10)
+
+
+var10 = ​not ​3 ​> ​1 ​and ​5 ​!= ​2 ​AND ​6 ​== ​6
+
+
+print(var10)
